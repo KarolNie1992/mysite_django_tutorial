0529bfcf83844519f6e9fd21c0aa446f110c4b86
Karol_Niedzwiedz <misiek1992.08@gmail.com>
2021-04-16 00:41:09 +0200

    end 4 and start 5 to (Obszerniejsze testy)

--- polls/views.py
+++ polls/views.py
@@ -5,20 +5,37 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
+
 from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
+class IndexView(generic.ListView):
     """
     Index for polls app
     """
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list,}
-    return render(request,'polls/index.html', context)
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    #context = {'latest_question_list': latest_question_list,}
+    #return render(request,'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions. """
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+#def detail(request, question_id):
     """
     Details for polls app
     https://docs.djangoproject.com/pl/3.2/intro/tutorial03/#a-shortcut-get-object-or-404
@@ -30,17 +47,16 @@ def detail(request, question_id):
         raise Http404 ("Question does not exist.") from question_doesnotexist
     return render(request,'polls/detail.html', {'question': question})
     """
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,'polls/detail.html', {
-        'question': question,
-        })
-        
-def results(request, question_id):
-    """
-    Results for polls app
-    """
-    question = get_object_or_404( Question, pk=question_id )
-    return render(request, 'polls/results.html', {'question': question,})
+ #   question = get_object_or_404(Question, pk=question_id)
+ #   return render(request,'polls/detail.html', {
+ #       'question': question,
+ #       })
+# def results(request, question_id):
+#     """
+#     Results for polls app
+#     """
+#     question = get_object_or_404( Question, pk=question_id )
+#     return render(request, 'polls/results.html', {'question': question,})
 
 def vote(request, question_id):
     """
