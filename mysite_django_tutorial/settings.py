2a05f0eb34d2346979904396aefe368bf0d4f085
Karol_Niedzwiedz <misiek1992.08@gmail.com>
2021-06-25 01:30:10 +0200

    dark mode fix

--- mysite_django_tutorial/settings.py
+++ mysite_django_tutorial/settings.py
@@ -71,6 +71,11 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 WSGI_APPLICATION = 'mysite_django_tutorial.wsgi.application'
 
 
@@ -122,6 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
